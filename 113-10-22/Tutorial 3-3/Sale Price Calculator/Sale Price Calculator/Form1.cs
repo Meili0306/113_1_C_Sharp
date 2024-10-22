f1b25d06a30b6c32ad7b68754d1668b411fc774c
Meili0306 <A113221046@mail.shu.edu.tw>
2024-10-23 00:04:28 +0800

    113-10-22

---
+++ 113-10-22/Tutorial 3-3/Sale Price Calculator/Sale Price Calculator/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sale_Price_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal originalPrice;
+            decimal discountPerentage;
+            decimal salePrice;
+        try
+            originalPrice = decimal.Parse(originalPriceTextBox.Text);
+            discountPerentage = decimal.Parse(discountPercentageTextBox.Text);
+            salePrice = originalPrice - (originalPrice * discountPerentage / 100);
+
+        }
+    }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
