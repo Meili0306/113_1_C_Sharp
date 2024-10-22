ec6486ae0d8f842348e188c640c4a2e099f5dca1
Meili0306 <A113221046@mail.shu.edu.tw>
2024-10-22 17:59:23 +0800

    113-10-22

---
+++ 113-10-22/GroupBoxDemo/GroupBoxDemo/Form1.cs
@@ -0,0 +1,20 @@
+namespace GroupBoxDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
