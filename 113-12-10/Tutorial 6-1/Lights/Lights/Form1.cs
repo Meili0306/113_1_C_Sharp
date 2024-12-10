43631fd7aad507826849a92940db4db07b9e53fd
Meili0306 <A113221046@mail.shu.edu.tw>
2024-12-10 17:57:37 +0800

    113-12-10

---
+++ 113-12-10/Tutorial 6-1/Lights/Lights/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lights
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void turnLigtOn()
+        {
+            lightOnPictureBox.Visible = true;
+            lightOffPictureBox.Visible = false;
+            lightStateLabel.Text = "ON";
+        }
+        private void turnLigtOff()
+        {
+            lightOnPictureBox.Visible = false;
+            lightOffPictureBox.Visible = true;
+            lightStateLabel.Text = "OFF";
+        }
+        private void switchButton_Click(object sender, EventArgs e)
+        {
+            if (lightOnPictureBox.Visible == true)
+            {
+                // 關燈
+                turnLigtOff();
+            }
+            else 
+            {
+                //開燈
+                turnLigtOn();
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+    }
+}
